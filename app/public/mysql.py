4268a2fc23fccea76273bbde5a37d82e08858549
mahaining <1072290021@qq.com>
2018-07-28 22:52:33 +0800

    更新 mysql.py

--- app/public/mysql.py
+++ app/public/mysql.py
@@ -2,8 +2,8 @@ import pymysql  # 导入 pymysql
 
 
 def get_db_con():
-    db = pymysql.connect (host="120.78.206.186", user="tecker",
-                          password="tck@2017", db="txyy_customer", port=3306, charset='utf8')
+    db = pymysql.connect (host="**********", user="****",
+                          password="*****", db="*******", port=3306, charset='utf8')
     return db
 
 
